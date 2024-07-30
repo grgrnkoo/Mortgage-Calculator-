36ff36ad254730942e0eec1ee785ad2ca01303b3
Oleg Grigorenko <grigorenko996@gmail.com>
2024-07-31 00:05:00 +0700

    Update EmptyFormResult.jsx

--- src/components/EmptyFormResult.jsx
+++ src/components/EmptyFormResult.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function EmptyFormResult() {
     return (
         <div className="emptyFormResult">
-            <img src="../../public/images/illustration-empty.svg" alt="Calculator Art.Waiting for data" />
+            <img src="https://raw.githubusercontent.com/grgrnkoo/Mortgage-Calculator-/b9d18dbeb45b0e9eaf2c73e0038e3daab1a5b455/public/images/illustration-empty.svg" alt="Calculator Art.Waiting for data" />
             <h3>Results shown here</h3>
             <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
         </div>
